ee55d908d1a9e2b6f41fdfb06f41d16b0c8a5870
Alvaro Morales <alvarom@mit.edu>
2014-02-07 09:36:54 -0800

    Add boostrap script and python syntax file.

---
+++ vim/vimify.sh
@@ -0,0 +1,10 @@
+sudo apt-get install git-core -y
+
+mkdir ~/.vim
+mkdir ~/.vim/bundle
+mkdir ~/.vim/ftplugin
+
+cp ftplugin/python.vim ~/.vim/ftplugin
+cp .vimrc ~
+
+git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
