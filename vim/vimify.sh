6856ce0ff9e8761dfa0cc64f8022f2a59c311b9a
Alvaro Morales <alvarom@mit.edu>
2015-04-26 15:27:39 -0700

    Clean up vimify

--- vim/vimify.sh
+++ vim/vimify.sh
@@ -1,15 +1,33 @@
-sudo apt-get install git-core -y
+#!/bin/bash
 
-mkdir -p ~/.vim
-mkdir -p ~/.vim/bundle
-mkdir -p ~/.vim/ftplugin
+echo "Starting Alvaro's vim setup"
 
-cp ftplugin/*.vim ~/.vim/ftplugin
-cp vimrc ~/.vimrc
+# Make sure git is installed
+OS_T="$OSTYPE"
+if [[ $OS_T == "linux-gnu" ]]; then
+  # Linux (Ubuntu)
+  sudo apt-get install vim -y
+  sudo apt-get install git -y
+  sudo apt-get install python-pip -y
+elif [[ $OS_T == "darwin"* ]]; then
+  # MAC OS
+  brew install git
+fi
 
+# Create the necessary directories
+[ -e ~/.vim ] || mkdir -p ~/.vim
+[ -e ~/.vim/bundle ] || mkdir -p ~/.vim/bundle
+[ -e ~/.vim/ftplugin ] || mkdir -p ~/.vim/ftplugin
+
+# Install Vundle
 if [ ! -e ~/.vim/bundle/vundle ]; then
   git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
 fi
 
 pip install pep8
 pip install autopep8
+
+cp ftplugin/*.vim ~/.vim/ftplugin
+cp vimrc ~/.vimrc
+
+echo "Done setting up vim"
