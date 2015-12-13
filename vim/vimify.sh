2277070ad0f7a80d9f757dea8f7437a671b67c21
Alvaro Morales <alvarom@mit.edu>
2015-12-13 13:48:57 -0500

    Switch to neovim

--- vim/vimify.sh
+++ vim/vimify.sh
@@ -2,18 +2,14 @@
 
 echo "Starting Alvaro's vim setup"
 
-# TODO: switch to neovim
-
-# Make sure git is installed
+# Install neovim and dependencies
 OS_T="$OSTYPE"
 if [[ $OS_T == "linux-gnu" ]]; then
   # Linux (Ubuntu)
-  sudo apt-get install vim -y
-  sudo apt-get install git -y
-  sudo apt-get install python-pip -y
+  sudo apt-get install git python-pip neovim -y
 elif [[ $OS_T == "darwin"* ]]; then
-  # MAC OS
-  brew install git
+  # Mac OS X
+  brew install git neovim
 fi
 
 # Create the necessary directories
@@ -26,12 +22,20 @@ if [ ! -e ~/.vim/bundle/vundle ]; then
   git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
 fi
 
+# Copy files
+cp ftplugin/*.vim ~/.vim/ftplugin
+cp vimrc ~/.vimrc
+
+# Setup virtualenv for neovim
+source `which virtualenvwrapper.sh`
+mkvirtualenv nvim
+pip install --upgrade pip autopep8 neovim pep8 pyflakes
+deactivate
+
+# TODO: change PYTHONPATH to nvim virtualenv instead of installing system-wide
 if ! $(python -c "import autopep8" &> /dev/null); then sudo pip install autopep8; fi;
 if ! $(python -c "import neovim" &> /dev/null); then sudo pip install neovim; fi;
 if ! $(python -c "import pep8" &> /dev/null); then sudo pip install pep8; fi;
 if ! $(python -c "import pyflakes" &> /dev/null); then sudo pip install pyflakes; fi;
-
-cp ftplugin/*.vim ~/.vim/ftplugin
-cp vimrc ~/.vimrc
 
 echo "Done setting up vim"
